64ecb8ef9ed129fc7bae5b15f08d3c4a36d42535
YunL <sam.looong@gmail.com>
2022-03-11 00:43:58 +1100

    bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
+environment.plugins.append(
+    'Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquary',
+        jQuery: 'jquary',
+        Popper: ['popper.js', 'default'],
+    })
+);
 
-module.exports = environment
+module.exports = environment;
