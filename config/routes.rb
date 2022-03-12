c11134a9a4b111c09389850cdf9bba9f1e375977
YunL <sam.looong@gmail.com>
2022-03-12 18:41:26 +1100

    add index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'games/index'
+  get 'games/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "pages#home"
   get '/restricted', to: "pages#restricted"
-  
+  get '/games', to: "games#index", as: 'games'
+
 end
