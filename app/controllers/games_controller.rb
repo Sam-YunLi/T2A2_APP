70bf147ad5ea8642c6d927a632d84909580edb96
YunL <sam.looong@gmail.com>
2022-03-13 18:23:15 +1100

    create edit and delete pages, and form build

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,68 @@
 class GamesController < ApplicationController
-  
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
+  before_action :set_form_vars, only: [:new, :edit]
+
+  # show all the games list
   def index
     @games = Game.all
   end
 
+  # show the details of the game
   def show
     @game = Game.find(params[:id])
   end
+
+  # create new game
+  def new
+    @game = Game.new
+  end
+
+  # 
+  def create
+    @game = current_user.games.new(game_params)
+    if @game.save
+      redirect_to @game, notice: "Game successfully created"
+    else
+      pp @game.errors
+      set_form_vars
+      render "new", notice: "Something went wrong"
+    end
+  end
+
+  # update the game information
+  def update
+    @game = update.(game_params)
+    if @game.save
+      redirect_to @game, notice: "Game successfully updated"
+    else
+      pp @game.errors
+      set_form_vars
+      render "edit", notice: "Something went wrong"
+    end
+  end
+
+  # delete the game form the game list
+  def destroy
+    @game.destory
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :price, :category_id, :condition, :description, :stock, :platform_id, :display)
+  end
+
+  def set_game
+
+  end
+
+  def set_form_vars
+    @categories = Category.all
+    @platforms = Platform.all
+    @conditions = Game.conditions.keys
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "pages#home"
+
   get '/restricted', to: "pages#restricted"
   get '/games', to: "games#index", as: 'games'
+
+  post '/games', to: "games#create"
+  get '/games/new', to: "games#new", as: 'new_game'
+
   get '/games/:id', to: "games#show", as: 'game'
+
+  put '/games/:id', to: "games#update"
+  patch '/games/:id', to: "games#update"
+
+  delete '/games/:id', to: "games#destroy"
+
+  get '/games/:id/edit', to: "games#edit", as: 'edit_game'
 
 end
