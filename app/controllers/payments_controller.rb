f8910d263ebe1af1afdf2a9601d8cc63a98fbe7f
YunL <sam.looong@gmail.com>
2022-03-14 23:27:26 +1100

    webhook signing secret setting

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,13 +6,33 @@ class PaymentsController < ApplicationController
   end
 
   def webhook
-    payment_intent_id = params[:data][:object][:payment_intent]    
+    # security for the webhook request, cos the authenticity tooken is skipped
+    begin
+      payload = request.raw_post
+      header = request.headers['HTTP_STRIPE_SIGNATURE']
+      secret = Rails.application.credentials.dig(:stripe, :webhook_signing_secret)
+      event = Stripe::Webhook.construct_event(payload, header, secret)
+    rescue Stripe::SignatureVerificationError => e
+      render jason: {error: "Unauthorised"}, status: 403
+      return
+    rescue JSON::ParserError => e
+      render jason: {error: "Bad request"}, status: 422
+      return
+    end
+
+    pp "******************"
+    pp event
+    pp "******************"
+
+    payment_intent_id = event.data.object.payment_intent    
     payment = Stripe::PaymentIntent.retrieve(payment_intent_id)
     game_id = payment.metadata.game_id
     pp payment.charges.data[0].receipt_url
     @game = Game.find(game_id)
     @game.stock -= 1
     @game.save
+    # create order and track the information
+    
   end
 
 end
